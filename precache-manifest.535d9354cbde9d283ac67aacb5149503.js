081d7e923a64cbdb3fc1b6683f223f83d7007f09
Luiza Avetisyan <l.avetisyan7777@gmail.com>
2020-02-02 14:35:28 +0400

    Updates

---
+++ precache-manifest.535d9354cbde9d283ac67aacb5149503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7b9b3d9ad75b9846f88b12fc25a5b5",
+    "url": "/colorchange/index.html"
+  },
+  {
+    "revision": "8bd6eba1a1d14c6637b7",
+    "url": "/colorchange/static/css/main.1e78c7c2.chunk.css"
+  },
+  {
+    "revision": "cdcf40c879511ac98358",
+    "url": "/colorchange/static/js/2.8ddfd22e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/colorchange/static/js/2.8ddfd22e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bd6eba1a1d14c6637b7",
+    "url": "/colorchange/static/js/main.7489bd57.chunk.js"
+  },
+  {
+    "revision": "c927d280cdeff0d81ca0",
+    "url": "/colorchange/static/js/runtime-main.87435bd4.js"
+  }
+]);
